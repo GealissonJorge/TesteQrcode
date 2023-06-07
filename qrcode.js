137c9d7c31446931a24be1ea26d07a1920339259
GealissonJorge <gealissonj@gmail.com>
2023-06-07 15:56:23 -0300

    first commit

---
+++ qrcode.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const qr= require('qr-image')
+
+app.set('view engine', 'ejs')
+
+app.get("/qrcode", (req, res) => {
+    const url = "https://www.google.com"
+    const code = qr.image(url, { type: 'svg' })
+    res.type('svg')
+    code.pipe(res)
+})
+app.get("/", (req, res) => {
+    res.render("leitor")
+})  
+
+
+app.listen(3000, () => {
+    console.log('listening on port 3000')
+})
