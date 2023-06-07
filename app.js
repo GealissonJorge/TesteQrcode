faf1336e7300be4b782ec911f3b4979716b73e3f
GealissonJorge <gealissonj@gmail.com>
2023-06-07 17:03:44 -0300

    changes to files

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ const app = express()
 const qr= require('qr-image')
 
 app.set('view engine', 'ejs')
-
+app.get('/', (req, res) => {
+    res.render('home')
+})
 app.get("/qrcode", (req, res) => {
     const url = "https://www.google.com"
     const code = qr.image(url, { type: 'svg' })
     res.type('svg')
     code.pipe(res)
 })
-app.get("/", (req, res) => {
+app.get("/leitor", (req, res) => {
     res.render("leitor")
 })  
 
 
-app.listen(process.env.PORT, () => {
+app.listen(3000, () => {
     console.log('listening on port 3000')
 })
